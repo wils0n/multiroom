2452c742c63126297bcf18c739ada8f62e69d5a8
wils0n <wiljm0.0@gmail.com>
2015-07-24 16:23:50 -0500

    add angular controller to login and register

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,27 @@
+(function(){
+	var app = angular.module("HomeApp", [])
+       .config(function($interpolateProvider){
+              $interpolateProvider.startSymbol('[[').endSymbol(']]');
+       });
 
-app.controller('loginController', function($scope){
-	console.log("holi")
-});
+	app.controller('loginController', function($scope){
+		$scope.MyLogin = {};
 
+		$scope.MyLogin.sendForm = function(){
+			console.log("email: ", $scope.MyLogin.email);
+			console.log("password: ", $scope.MyLogin.password);
+		};
+	});
+
+	app.controller('registerController', function($scope){
+		$scope.MyRegister = {};
+
+		$scope.MyRegister.sendForm = function(){
+			console.log("First Name: ", $scope.MyRegister.firstName);
+			console.log("Last Name: ", $scope.MyRegister.lastName);
+			console.log("Email: ", $scope.MyRegister.email);
+			console.log("Password: ", $scope.MyRegister.password);
+		};
+	});
+
+})();
